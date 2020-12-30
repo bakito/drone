2dad4400382b238ebeda250161f8ae9108b7b97c
bakito <github@bakito.ch>
2020-12-30 17:18:20 +0100

    support versioning for stash

--- cmd/drone-server/config/config.go
+++ cmd/drone-server/config/config.go
@@ -399,6 +399,7 @@ type (
 		PrivateKey     string `envconfig:"DRONE_STASH_PRIVATE_KEY"`
 		SkipVerify     bool   `envconfig:"DRONE_STASH_SKIP_VERIFY"`
 		Debug          bool   `envconfig:"DRONE_STASH_DEBUG"`
+		Version        string `envconfig:"DRONE_STASH_VERSION"`
 	}
 
 	// S3 provides the storage configuration.
--- cmd/drone-server/inject_client.go
+++ cmd/drone-server/inject_client.go
@@ -195,7 +195,7 @@ func provideStashClient(config config.Config) *scm.Client {
 		logrus.WithError(err).
 			Fatalln("main: cannot parse the Stash Private Key")
 	}
-	client, err := stash.New(config.Stash.Server)
+	client, err := stash.NewVersioned(config.Stash.Server, config.Stash.Version)
 	if err != nil {
 		logrus.WithError(err).
 			Fatalln("main: cannot create the Stash client")
